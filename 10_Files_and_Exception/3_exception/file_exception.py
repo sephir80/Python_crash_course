19c262ca07948e3c504e72b761ccb9e9a895b12a
Alessandro Tognon <31048534+sephir80@users.noreply.github.com>
2022-02-07 13:21:24 +0100

    made general function

--- 10_Files_and_Exception/3_exception/file_exception.py
+++ 10_Files_and_Exception/3_exception/file_exception.py
@@ -1,12 +1,19 @@
-filename = 'testo.txt'
-## alice nel paese delle meraviglie
-try:
-    with open(filename, encoding='utf-8') as f:
-        contents = f.read()
-except FileNotFoundError:
-    print(f'Il file {filename} non esiste.')
-else:
-    #conta il numero di parole nel file
-    parole = contents.split()
-    num_parole = len(parole)
-    print(f'Il file {filename} è formato da {num_parole} parole')
+def count_words(nomefile: str):
+    """
+
+    :type nomefile: str
+    """
+    try:
+        with open(nomefile, encoding='utf-8') as f:
+            contents = f.read()
+    except FileNotFoundError:
+        print(f'Il file {nomefile} non esiste.')
+    else:
+        # conta il numero di parole nel file
+        parole = contents.split()
+        num_parole = len(parole)
+        print(f'Il file {nomefile} è formato da {num_parole} parole')
+
+
+nomefile = 'testo.txt'
+count_words(nomefile)
