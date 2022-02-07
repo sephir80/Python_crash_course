a4bb9b2f77c92646820f55fd2899bac7deeb539a
Alessandro Tognon <31048534+sephir80@users.noreply.github.com>
2022-02-07 14:43:05 +0100

    add not exist book and comment if you want silently error

--- 10_Files_and_Exception/3_exception/file_exception.py
+++ 10_Files_and_Exception/3_exception/file_exception.py
@@ -7,7 +7,7 @@ def count_words(filename: str):
         with open(filename, encoding='utf-8') as f:
             contents = f.read()
     except FileNotFoundError:
-        print(f'Il file {filename} non esiste.')
+        print(f'Il file {filename} non esiste.')    ##failing silently use pass
     else:
         # conta il numero di parole nel file
         parole = contents.split()
@@ -15,7 +15,6 @@ def count_words(filename: str):
         print(f'Il file {filename} è formato da {num_parole} parole')
 
 
-filenames = ['alice.txt', 'empire.txt', 'migrant.txt']
+filenames = ['alice.txt', 'empire.txt', 'migrant.txt', 'fail.txt']
 for filename in filenames:
     count_words(filename)
-
