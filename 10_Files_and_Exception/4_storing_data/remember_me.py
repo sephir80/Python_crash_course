f2d10eec49f8eeed0181c84399ceec1c35485839
Alessandro Tognon <alessandro.tognon@flamespray.org>
2022-02-11 23:40:33 +0100

    refactor 3 level three function

--- 10_Files_and_Exception/4_storing_data/remember_me.py
+++ 10_Files_and_Exception/4_storing_data/remember_me.py
@@ -13,17 +13,24 @@ def get_stored_username():
         return username
 
 
+def get_new_username():
+    """Richiedi il nome utente"""
+    username = input("come ti chiami? ")
+    filename = 'username.json'
+    with open(filename, 'w') as f:
+        json.dump(username, f)
+    return username
+
+
+
 def saluta_utente():
     """Saluta utente per nome"""
     username = get_stored_username()
     if username:
         print(f"Bentornato , {username}")
     else:
-        username = input("come ti chiami? ")
-        filename = 'username.json'
-        with open(filename, 'w') as f:
-            json.dump(username, f)
-            print(f"Il tuo nome <{username}> è stato salvato! ")
+        username = get_new_username()
+        print(f"Ci ricorderemo di te quando torni , {username}")
 
 
 saluta_utente()
