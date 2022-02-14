9915920cf4ce80961fb04c7d45b4635b3857250c
Alessandro Tognon <alessandro.tognon@flamespray.org>
2022-02-14 15:55:34 +0100

    metodo setUp della classe unittest.testcase

--- 11_test_code/2_class/test_survey.py
+++ 11_test_code/2_class/test_survey.py
@@ -1,26 +1,30 @@
 import unittest
+
 from survey import AnonymousSurvey
+
 
 class TestAnonymousSurvey(unittest.TestCase):
     """Tests for the class AnonymousSurvey"""
 
+    def setUp(self):
+        """Crea un questionario e imposta un set di risposte da usare con tutti i metodi"""
+        question = "Quale linguaggio hai imparato per prima?"
+        self.my_survey = AnonymousSurvey(question)
+        self.responses = ['English', 'Italian', 'Mandarin']
+
     def test_store_single_response(self):
         """Test per una singola risposta."""
-        question = "Quale linguaggio hai imparato per prima?"
-        my_survey = AnonymousSurvey(question)
-        my_survey.store_response('English')
-        self.assertIn('English', my_survey.responses)
+        self.my_survey.store_response(self.responses[0])
+        self.assertIn(self.responses[0], self.my_survey.responses)
 
     def test_store_three_responses(self):
         """Test that three individual responses are stored properly."""
-        question = "Quale linguaggio hai imparato per prima?"
-        my_survey = AnonymousSurvey(question)
-        responses = ['English', 'Spanish', 'Mandarin']
-        for response in responses:
-            my_survey.store_response(response)
+        for response in self.responses:
+            self.my_survey.store_response(response)
 
-        for response in responses:
-            self.assertIn(response, my_survey.responses)
+        for response in self.responses:
+            self.assertIn(response, self.my_survey.responses)
+
 
 if __name__ == '__main__':
     unittest.main()
